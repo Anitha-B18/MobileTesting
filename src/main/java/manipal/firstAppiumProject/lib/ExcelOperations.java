b0912b900e258b48ee7d4f685fa2177a66ea843d
Anitha-B18 <anithb15@in.ibm.com>
2020-09-30 15:02:27 +0530

    excel operations added.

---
+++ src/main/java/manipal/firstAppiumProject/lib/ExcelOperations.java
@@ -0,0 +1,63 @@
+package manipal.firstAppiumProject.lib;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelOperations {
+	
+	XSSFWorkbook wb;
+	XSSFSheet sheet1;
+
+	public ExcelOperations(String filepath) 
+	{
+		try 
+		{
+			File src = new File(filepath);
+			FileInputStream fis = new FileInputStream(src);
+			wb = new XSSFWorkbook(fis);
+
+		} 
+		catch (Exception e) 
+		{
+
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public String getData(int sheet_index, int row_index, int col_index) 
+	{
+		
+		return wb.getSheetAt(sheet_index).getRow(row_index).getCell(col_index).getStringCellValue();
+	}
+	
+	public void setData(String sheetname,int rownum,int colnum,String result) throws IOException
+	{
+		sheet1 = wb.getSheet(sheetname);
+		sheet1.getRow(rownum).getCell(colnum).setCellValue(result);
+		try 
+		{
+			   FileOutputStream fos = new FileOutputStream(".\\Test Data\\User Details.xlsx"); // saves xls file to                                                                                                             disk
+			   wb.write(fos);
+			   fos.close();  
+		}
+		catch (FileNotFoundException e) 
+		{   
+			   e.printStackTrace();
+		} 
+	}
+	
+	
+	public String getData1(int sheet_index, int row_index, int col_index) 
+	{
+		
+		return wb.getSheetAt(sheet_index).getRow(row_index).getCell(col_index).getStringCellValue();
+	}
+}
+
+
--- src/test/java/manipal/firstAppiumProject/NewUserRegistration.java
+++ src/test/java/manipal/firstAppiumProject/NewUserRegistration.java
@@ -15,10 +15,12 @@ import org.testng.annotations.Test;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import manipal.firstAppiumProject.lib.ExcelOperations;
 
 public class NewUserRegistration {
 	
 	AndroidDriver driver;
+	ExcelOperations excelObject = new ExcelOperations(".\\Test Data\\User Details.xlsx");
 	
 	@BeforeTest
 	public void setUp() throws MalformedURLException, InterruptedException
@@ -39,12 +41,16 @@ public class NewUserRegistration {
 		driver.findElement(MobileBy.xpath("//*[@href='/register']")).click();
 		WebDriverWait wait = new WebDriverWait(driver,40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(),'Register')]")));
+		String fname = excelObject.getData(0, 0, 0);
+		String lname = excelObject.getData(0, 0, 1);
+		String email = excelObject.getData(0, 0, 2);
+		String password = excelObject.getData(0, 0, 3);
 		driver.findElement(MobileBy.xpath("//*[@value='F']")).click();
-		driver.findElement(MobileBy.xpath("//input[@id='FirstName']")).sendKeys("Anitha");
-		driver.findElement(MobileBy.xpath("//input[@id='LastName']")).sendKeys("Rao");
-		driver.findElement(MobileBy.xpath("//input[@id='Email']")).sendKeys("qwe.rty5671@gmail.com");
-		driver.findElement(MobileBy.xpath("//input[@id='Password']")).sendKeys("password123$");
-		driver.findElement(MobileBy.xpath("//input[@id='ConfirmPassword']")).sendKeys("password123$");
+		driver.findElement(MobileBy.xpath("//input[@id='FirstName']")).sendKeys(fname);
+		driver.findElement(MobileBy.xpath("//input[@id='LastName']")).sendKeys(lname);
+		driver.findElement(MobileBy.xpath("//input[@id='Email']")).sendKeys(email);
+		driver.findElement(MobileBy.xpath("//input[@id='Password']")).sendKeys(password);
+		driver.findElement(MobileBy.xpath("//input[@id='ConfirmPassword']")).sendKeys(password);
 		driver.findElement(MobileBy.xpath("//input[@value='Register']")).click();
 		Thread.sleep(3000);
 		Assert.assertTrue(driver.findElement(MobileBy.xpath("//*[contains(text(),'Your registration completed') ]")).isDisplayed(), "Registration successfull");
